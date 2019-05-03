e9027b28ee20518a72d64c8201ae3849dc3e9a6a
mz <zmyunsw@gmail.com>
2019-05-03 17:43:16 +0930

    phase 5 finished

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,9 +1,13 @@
 import React from "react"
 
 function TodoItem(props) {
+    function handleCheck() {
+        console.log("checked!")
+    }
+
     return (
         <div className="todo-item">
-            <input type="checkbox" />
+            <input type="checkbox" onChange={handleCheck}/>
             <p>{props.todo.text}</p>
         </div>
     )
