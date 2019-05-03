806bf2ebd3f760650ac2f565ec031c35450a6a96
mz <zmyunsw@gmail.com>
2019-05-03 10:48:43 +0930

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from "react"
-
-import Footer from "./Footer"
-import Header from "./Header"
-import MainContent from "./MainContent"
+import ContactCard from "./ContactCard"
 
 function App() {
     return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
+        <div className="contacts">
+            <ContactCard 
+                name="mz"
+                imgUrl=""
+            />
+
+            <ContactCard 
+                name="mz"
+                imgUrl=""
+            />
+
+            <ContactCard 
+                name="mz"
+                imgUrl=""
+            />
+            
+            <ContactCard 
+                name="mz"
+                imgUrl=""
+            />
         </div>
     )
 }
---
+++ src/ContactCard.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function ContactCard(props) {
+    return (
+        <div className="contact-card">
+            <img src={props.imgUrl} />
+            <h3>{props.name}</h3>
+        </div>
+    )
+}
+
+export default ContactCard
