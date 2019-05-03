2553a2d29b927c99fca895f536c74e144f132013
mz <zmyunsw@gmail.com>
2019-05-03 10:58:32 +0930

    v1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
+/* 
+Given a list of products (as an array of objects, as seen in productsData.js)
+render a <Product /> component (which you'll also need to create) for each
+product in the list.
+
+Make sure to use the array's `.map()` method to create these components, and 
+don't forget to pass a `key` prop to it to avoid the warning.
+*/
+
 import React from "react"
-import ContactCard from "./ContactCard"
+import productsData from "./vschoolProducts"
 
 function App() {
-    return (
-        <div className="contacts">
-            <ContactCard 
-                name="mz"
-                imgUrl=""
-            />
-
-            <ContactCard 
-                name="mz"
-                imgUrl=""
-            />
-
-            <ContactCard 
-                name="mz"
-                imgUrl=""
-            />
-            
-            <ContactCard 
-                name="mz"
-                imgUrl=""
-            />
-        </div>
-    )
+    const productsComponents = productsData.map(product => 
+        <div key={product.id}>
+            <h3>{product.name}</h3>
+            <ul>
+                <li>price: {product.price}</li>
+                <li>description: {product.description}</li>
+            </ul>
+        </div>    
+    ) 
+  return (
+    <div>
+        {productsComponents}
+    </div>
+  )
 }
 
 export default App
