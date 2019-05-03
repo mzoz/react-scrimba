b7eac205c564ae728d224566672dfd6f45da679a
mz <zmyunsw@gmail.com>
2019-05-03 12:24:55 +0930

    without conditional checked

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react"
 import TodoItem from "./TodoItem"
-
+import todosData from "./todosData"
 import "./style.css"
 
 function App() {
+    const todosComponents = todosData.map(todo =>
+        <TodoItem key={todo.id} todo={todo} />
+    )
+
     return (
         <div className="todo-list">
-            <TodoItem />
+            {todosComponents}
         </div>
     )
 }
