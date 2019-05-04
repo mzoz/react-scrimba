33a0e7053b0fff444d6273380721ba6cdb1cc5a1
mz <zmyunsw@gmail.com>
2019-05-04 12:56:16 +0930

    change state

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            answer: "yes"
+            count: 0
         }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState({count: 1})
     }
 
     render() {
         return (
             <div>
-                <h1>{this.state.name}</h1>
-                <h3>{this.state.age} years old</h3>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.handleClick}>Change!</button>
             </div>
         )
     }
