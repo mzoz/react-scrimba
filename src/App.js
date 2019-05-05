60e41560eff08a8113482c79b4f28d2260f4d7e1
mz <zmyunsw@gmail.com>
2019-05-06 00:42:26 +0930

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, {Component} from "react"
+import Conditional from "./Conditional"
 
 class App extends Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            isLoading: true
+        }
     }
 
     componentDidMount() {
-        
+        setTimeout(() => {
+            this.setState({
+                isLoading: false
+            })
+        }, 1500)
     }
 
     render() {
         return (
             <div>
-                
+                {this.state.isLoading ?
+                <h1>Loading...</h1> :
+                <Conditional />}
             </div>
         )
     }
