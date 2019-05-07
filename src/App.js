1dbb7d09cf2746fb5560be15c9d54cbf2ec5a989
mz <zmyunsw@gmail.com>
2019-05-07 21:41:38 +0930

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, {Component} from "react"
 
+// https://swapi.co/
+// promise
+
 class App extends Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            loading: false,
+            character: {}
+        }
     }
 
-    render() {
+    componentDidMount() {
+        this.setState({loading: true})
+        fetch("https://swapi.co/api/people/1")
+            .then(response => response.json())
+            .then(data => this.setState({character: data, loading: false}))
+    }
+
+
+
+    render(data) {
         return (
             <div>
-                
+                {this.state.loading ? "loading" : null}
+                {this.state.character.name}
             </div>
         )
     }
