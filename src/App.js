430215ea6dc8ec07a91c17bd128f74c40b857def
mz <zmyunsw@gmail.com>
2019-05-09 01:51:45 +0930

    part1

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import React, {Component} from "react"
 class App extends Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {first: "", last: ""}
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value})
     }
 
     render() {
         return (
             <div>
-                
+                <form>
+                    <input type="text" name="first" value={this.state.first} placeholder="First name" onChange={this.handleChange} />
+                    <br /   >
+                    <input type="text" name="last" value={this.state.last} placeholder="Last name" onChange={this.handleChange} />
+                </form>
+                <p>{this.state.first} {this.state.last}</p>
             </div>
         )
     }
