27ffe4cafe2138413847d66ff2fa64308579e626
mz <zmyunsw@gmail.com>
2019-05-09 18:06:19 +0930

    update

--- src/App.js
+++ src/App.js
@@ -1,61 +1,39 @@
 import React, {Component} from "react"
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {first: "", last: "", isFriendly: true, gender: ""}
+    constructor(props) {
+        super(props)
+        this.state = {
+            first: "", 
+            last: "", 
+            age: null, 
+            gender: null,
+            destination: "",
+            dietaryRestrictions: []
+        }
         this.handleChange = this.handleChange.bind(this)
     }
 
     handleChange(event) {
-        console.log(event.target)
-        const {name, value, type, checked} = event.target
-        type === "checkbox" ? this.setState({[name]: checked}) : 
-                              this.setState({[name]: value})
+        const {name, value, type} = event.target
+        console.log(name, value, type)
+        // type === "checkbox" ? this.setState({[name]: checked}) : 
+        //                       this.setState({[name]: value})
     }
 
     render() {
         return (
             <div>
                 <form>
-                    <input type="text" name="first" value={this.state.first} placeholder="First name" onChange={this.handleChange} />
-                    <br /   >
-                    <input type="text" name="last" value={this.state.last} placeholder="Last name" onChange={this.handleChange} />
-                    <p>{this.state.first} {this.state.last}</p>
-
-                    <textarea />
-
+                    <input type="text" name="first" value={this.state.first} onChange={this.handleChange} />
+                    <input type="text" name="last" value={this.state.last} onChange={this.handleChange} />
+                    <input type="number" name="age" value={this.state.age} onChange={this.handleChange} />
                     <br />
-                    <label>
-                        <input 
-                            type="checkbox" 
-                            name="isFriendly"
-                            checked={this.state.isFriendly} 
-                            onChange={this.handleChange}
-                            /> Is friendly?
-                    </label>  
 
-                    <br />
-                    <label>
-                        <input 
-                            type="radio" 
-                            name="gender"
-                            value="male"
-                            checked={this.state.gender === "male"} 
-                            onChange={this.handleChange}
-                            /> Male
-                    </label> 
-
-                    <br />
-                    <label>
-                        <input 
-                            type="radio" 
-                            name="gender"
-                            value="female"
-                            checked={this.state.gender === "female  "} 
-                            onChange={this.handleChange}
-                            /> Female
-                    </label>   
+                    <select multiple value={this.state.gender} onChange={this.handleChange}>
+                        <option value="male">male</option>
+                        <option value="femail">femail</option>
+                    </select>
                 </form>
             </div>
         )
