cb8f18d0fb79be6c0073d4786bd636b2e84d9cd5
mz <zmyunsw@gmail.com>
2019-05-10 22:13:08 +0930

    finished

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, {Component} from "react"
+import React from "react"
+import Header from "./Header"
+import MemeGenerator from "./MemeGenerator"
+import "./style.css"
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {}
-    }
-
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+function App() {
+    return (
+        <div>
+            <Header />
+            <MemeGenerator />
+        </div>
+    )
 }
 
 export default App
